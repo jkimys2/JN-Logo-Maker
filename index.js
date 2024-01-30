0af3653c66274f4f07ea6a44381820f5f085157c
Jordan <jkimys2@gmail.com>
2024-01-30 16:23:36 -0500

    questions update

---
+++ index.js
@@ -0,0 +1,52 @@
+// packages required for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "letter",
+    message: "Please enter 1-3 characters for your logo!",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("You must type in 1-3 characters!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "letterColor",
+    message: "Enter a text color",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please choose a color!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "Please choose a shape:",
+    choices: ["Circle", "Square", "Triangle"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Enter a color for your logo shape!",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please choose a color!");
+        return false;
+      }
+    },
+  },
+];
